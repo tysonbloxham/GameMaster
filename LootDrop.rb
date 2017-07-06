9de63e50a1822e19cbc2adca8c52711116eb8603
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-07-06 03:47:47 -0600

    Added manufacturers to weapon drops

--- LootDrop.rb
+++ LootDrop.rb
@@ -1,32 +1,36 @@
 require 'colorize'
 
 def common_items
-	puts "You found a common item."
+	common_guns = ["Jacobs", "Dahl"]
+	puts "You found a common #{common_guns.sample} weapon."
 end
 
 def uncommon_items
-	puts "You found an uncommon item. :{".green
+	uncommon_guns = ["Torgue", "Vladof", "Jacobs", "Dahl"]
+	puts "You found an uncommon #{uncommon_guns.sample} weapon. :{".green
 end
 
 def rare_items
-	puts "You found a rare item. :)".blue
+	rare_guns = ["Torgue", "Vladof", "Jacobs", "Dahl", "Maliwan", "Tediore"]
+	puts "You found a rare #{rare_guns.sample} weapon. :)".blue
 end
 
 def legendary_items
-		puts "You found a legendary item, you lucky duck!".cyan
+		legendary_guns = ["Conference Call", "Hellfire", "Maggie"]
+		puts "You found a #{legendary_guns.sample}, you lucky duck!".cyan
 end
 
 
 def rarity_pool
-	rand_no = rand(0..100)
-	puts rand_no
-	if rand_no <= 60
+	rand_num = rand(0..100)
+	puts rand_num
+	if rand_num <= 50
 		common_items
-	elsif rand_no <= 85
+	elsif rand_num <= 80
 		uncommon_items
-	elsif rand_no <= 95
+	elsif rand_num <= 95
 		rare_items
-	elsif rand_no <= 100
+	elsif rand_num <= 100
 		legendary_items
 	else
 		puts "Something has gone horribly wrong...".red
