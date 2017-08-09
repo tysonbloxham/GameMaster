f7d3c82f0911a67479bcb78e71b2aae250e2ab05
Ian Earley <iearley88@gmail.com>
2017-08-09 14:12:46 -0600

    Created character select page with submission ability

--- BunkersAndBadasses/config/routes.rb
+++ BunkersAndBadasses/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :characters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post "/characters/submit" => "characters#create"
 end
