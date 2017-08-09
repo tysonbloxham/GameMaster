d56ed740aaed2f377f6e75034f25310cb498b59c
Tyson Bloxham <tysonsbloxham@gmail.com>
2017-08-09 14:13:04 -0600

    added individual class entries to seeds, customized show page for each class

--- BunkersAndBadasses/db/seeds.rb
+++ BunkersAndBadasses/db/seeds.rb
@@ -1,5 +1,9 @@
-3.times do |character|
-	Character.create!(character_name: "Character #{character + 1}", level: (character + 1), experience: 40 * (character + 1), health: 100, action_points: 10, badass_points: 10, evasion: 60, character_class: "Gunzerker", current_health: 80)
-end
+Character.create!(character_name: "Salvador", level: 1, experience: 40, health: 100, action_points: 10, badass_points: 10, evasion: 60, character_class: "Gunzerker", current_health: 10)
+Character.create!(character_name: "Zer0", level: 2, experience: 60, health: 100, action_points: 10, badass_points: 10, evasion: 60, character_class: "Assassin", current_health: 30)
+Character.create!(character_name: "Axton", level: 3, experience: 80, health: 100, action_points: 10, badass_points: 10, evasion: 60, character_class: "Commando", current_health: 50)
+Character.create!(character_name: "Gaige", level: 4, experience: 100, health: 100, action_points: 10, badass_points: 10, evasion: 60, character_class: "Mechromancer", current_health: 70)
+Character.create!(character_name: "Maya", level: 5, experience: 120, health: 100, action_points: 10, badass_points: 10, evasion: 60, character_class: "Siren", current_health: 80)
+Character.create!(character_name: "Krieg", level: 6, experience: 140, health: 100, action_points: 10, badass_points: 10, evasion: 60, character_class: "Psycho", current_health: 80)
 
-puts "3 characters created!"
+
+puts "6 characters created!"
