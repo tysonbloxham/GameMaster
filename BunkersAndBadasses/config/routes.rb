ba03559c0eceb2926d47a6c08c75410f3cf699f0
Ian Earley <iearley88@gmail.com>
2017-08-10 10:00:39 -0600

    updating files

--- BunkersAndBadasses/config/routes.rb
+++ BunkersAndBadasses/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :characters
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post "/characters/submit" => "characters#create"
-
-  root to: 'characters#index'
+  post "/characters/create" => "characters#create"
+  root to: "characters#index"
 end
